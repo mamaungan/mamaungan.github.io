c023b594426c3c4b1066baeaa55288a63c7094fb
mamaungan <mamaungan@gmail.com>
2024-08-08 01:52:37 +0700

    Update corelab.js

--- corelab.js
+++ corelab.js
@@ -1,4 +1,4 @@
-var i=0;var interval_id=0;$(document).ready(function(){
+var i=0;var interval_id=0;$(document).ready(function(){alert('ok');
   var a='<img src="https://mamaungan.github.io/unisa.jpg"><h1>Unisa Bandung</h1><p>Universitas Aisyiyah Bandung</p><a href="#" id="a">Klik untuk melanjutkan</a><i id="i"></i>';alert(a);
   $('center').html(a);
   alert(a);
